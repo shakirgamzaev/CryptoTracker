6f2c244e4de4b70b0b72aa0ac7a8c7b88e408575
Shakir Gamzaev <shkir100@gmail.com>
2024-11-06 13:48:54 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by shakir Gamzaev on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
